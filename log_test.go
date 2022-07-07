ef768e26dd980663d844a81060ae8276f9d7449c
Serhii Mariiekha <heartwilltell@gmail.com>
2022-07-07 15:34:38 +0200

    Add more options

--- log_test.go
+++ log_test.go
@@ -1,6 +1,8 @@
 package log
 
 import (
+	"bytes"
+	"fmt"
 	"os"
 	"reflect"
 	"strings"
@@ -81,6 +83,94 @@ func TestNew(t *testing.T) {
 	})
 }
 
+func TestLevelPrinting(t *testing.T) {
+	type tcase struct {
+		level Level
+	}
+
+	tests := map[string]tcase{
+		"DBG": {level: DBG},
+		"INF": {level: INF},
+		"WRN": {level: WRN},
+		"ERR": {level: ERR},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			var w bytes.Buffer
+			log := New(WithLevel(tc.level), WithWriter(&w))
+			log.Debug("debug")
+			log.Info("info")
+			log.Warning("warning")
+			log.Error("error")
+
+			lines := strings.Split(w.String(), "\n")
+			lines = lines[0 : len(lines)-1]
+
+			t.Log(lines)
+
+			switch tc.level {
+			case ERR:
+				if len(lines)-int(ERR) != 1 {
+					t.Errorf("Unexpected number of printed lines: want := %d got := %d", ERR+1, len(lines)-int(ERR))
+				}
+
+				for _, l := range lines {
+					if strings.HasPrefix(l, "\u001B[31mERR\u001B[0m:") {
+						continue
+					}
+
+					t.Errorf("Unexpected printed line! Only ERR logs are alowed in this case. Got := %s", w.String())
+				}
+
+			case WRN:
+				if len(lines)-int(WRN) != 1 {
+					t.Errorf("Unexpected number of printed lines: want := %d got := %d", WRN+1, len(lines)-int(WRN))
+				}
+
+				for _, l := range lines {
+					if strings.HasPrefix(l, "\u001B[31mERR\u001B[0m:") ||
+						strings.HasPrefix(l, "\u001B[33mWRN\u001B[0m:") {
+						continue
+					}
+
+					t.Errorf("Unexpected printed line! Only ERR and WRN logs are alowed in this case. Got := %s", w.String())
+				}
+			case INF:
+				if len(lines)-int(INF) != 1 {
+					t.Errorf("Unexpected number of printed lines: want := %d got := %d", INF+1, len(lines)-int(INF))
+				}
+
+				for _, l := range lines {
+					if strings.HasPrefix(l, "\u001B[31mERR\u001B[0m:") ||
+						strings.HasPrefix(l, "\u001B[33mWRN\u001B[0m:") ||
+						strings.HasPrefix(l, "\u001B[32mINF\u001B[0m:") {
+						continue
+					}
+
+					t.Errorf("Unexpected printed line! Only ERR WRN INF logs are alowed in this case. Got := %s", w.String())
+				}
+			case DBG:
+				if len(lines)-int(DBG) != 1 {
+					t.Errorf("Unexpected number of printed lines: want := %d got := %d", DBG+1, len(lines)-int(DBG))
+				}
+
+				for _, l := range lines {
+					if strings.HasPrefix(l, "\u001B[31mERR\u001B[0m:") ||
+						strings.HasPrefix(l, "\u001B[33mWRN\u001B[0m:") ||
+						strings.HasPrefix(l, "\u001B[32mINF\u001B[0m:") ||
+						strings.HasPrefix(l, "\u001B[35mDBG\u001B[0m:") {
+						continue
+					}
+
+					t.Errorf("Unexpected printed line! Only ERR WRN INF logs are alowed in this case. Got := %s", w.String())
+				}
+			}
+		})
+	}
+
+}
+
 func TestNewNopLog(t *testing.T) {
 	t.Run("Reflect type", func(t *testing.T) {
 		want := reflect.TypeOf(NopLog{})
@@ -90,6 +180,39 @@ func TestNewNopLog(t *testing.T) {
 			t.Errorf("Type mismatch; got := %v; want := %v", got, want)
 		}
 	})
+}
+
+func TestParseLevel(t *testing.T) {
+	type tcase struct {
+		str     string
+		want    Level
+		wantErr error
+	}
+
+	tests := map[string]tcase{
+		"OK info level":       {str: "info", want: INF, wantErr: nil},
+		"OK debug level":      {str: "debug", want: DBG, wantErr: nil},
+		"OK warning level":    {str: "warning", want: WRN, wantErr: nil},
+		"OK error level":      {str: "error", want: ERR, wantErr: nil},
+		"OK uppercase":        {str: "INFO", want: INF, wantErr: nil},
+		"Error empty string":  {str: "", want: INF, wantErr: ErrParseLevel},
+		"Error invalid level": {str: "invalid-level", want: 0, wantErr: fmt.Errorf("invalid-level %w", ErrParseLevel)},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := ParseLevel(tc.str)
+			if tc.wantErr != nil {
+				if !reflect.DeepEqual(err, tc.wantErr) {
+					t.Errorf("Error mismatch; got := %v; want := %v", err, tc.wantErr)
+				}
+			} else {
+				if !reflect.DeepEqual(got, tc.want) {
+					t.Errorf("Value mismatch; got := %v; want := %v", got, tc.want)
+				}
+			}
+		})
+	}
 }
 
 func TestLevel_String(t *testing.T) {
